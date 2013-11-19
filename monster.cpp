5cb8aa67dcedb011d916051a59c553b458aa8651
Geoffry Song <goffrie@gmail.com>
2013-11-19 11:15:42 -0500

    Have monsters wander.
    This is really pointless. Probably there needs to be a system to make
    the player move faster than most monsters, or else it's way too hard to
    do anything.

--- monster.cpp
+++ monster.cpp
@@ -10,18 +10,47 @@
 using namespace std;
 
 void Monster::step() {
+    Level* level = getLevel();
+
+    // Look for an adjacent player.
     struct IsPlayerVisitor : public LevelObjectVisitor {
-        Monster* m;
+        Player* player;
         void visit(Player& p) {
-            // XXX: check hostile
-            m->attack(&p);
+            player = &p;
         }
     };
     IsPlayerVisitor v;
-    v.m = this;
-    vector<LevelObject*> adj = getLevel()->neighbours(getY(), getX());
+    v.player = 0;
+    vector<LevelObject*> adj = level->neighbours(getY(), getX());
     for (unsigned int i = 0; i < adj.size(); ++i) {
         adj[i]->accept(v);
+    }
+    // Decide what to do.
+    if (v.player) {
+        // Attack the player.
+        // XXX: check hostile
+        attack(v.player);
+    } else {
+        // Wander.
+
+        // Look for a free location.
+        vector<pair<int, int> > locations;
+        for (int dy = -1; dy <= 1; ++dy) {
+            for (int dx = -1; dx <= 1; ++dx) {
+                if (dy == 0 && dx == 0) continue;
+                const int ny = getY() + dy,
+                          nx = getX() + dx;
+                if (level->free(ny, nx)) {
+                    locations.push_back(make_pair(ny, nx));
+                }
+            }
+        }
+
+        // Pick one at random.
+        if (locations.size() > 0) {
+            pair<int, int> newLocation = locations[rand() % locations.size()];
+            moveTo(newLocation.first, newLocation.second);
+        }
     }
 }
 
