6bb51c7c4178fc1905c4c2aaef23a1b657cf6730
Geoffry Song <goffrie@gmail.com>
2013-11-30 14:40:12 -0500

    copy editing

--- popup/popupcreator.cpp
+++ popup/popupcreator.cpp
@@ -14,7 +14,7 @@ void PopUpCreator::victory(int score) {
     line << "As you smash the second altar, all the monsters immediately turn to dust. You arise from the cave (after "
          "spending the next month trying to find your way back up) to the cheers and surprise of your village! It "
          "turns out that the merchant's guild summoned the monsters to drive up the price of weapons, then sell "
-         "them for exhorbitant prices. You saved the world and destroyed the capitalist plot!\n\n"
+         "them for exorbitant prices. You saved the world and destroyed the capitalist plot!\n\n"
          "Score: " << score;
     PopUp::make(line.str());
 }
@@ -22,7 +22,7 @@ void PopUpCreator::victory(int score) {
 void PopUpCreator::defeat(int score) {
     ostringstream line;
 
-    line << "You died, but honestly no-one in your village actually expected you to succeed. They had all fled to "
+    line << "You died, but honestly, no one in your village actually expected you to succeed. They had all fled to "
          "other continents and used you to distract the monsters until they could escape. So in a way, you "
          "succeeded! But not really.\n\n"
          "On the upside, you got " << score << " points!";
@@ -37,24 +37,24 @@ void PopUpCreator::level(int l) {
             line << "One month ago, monsters long thought extinct began appearing around your town. As more and more "
                 "powerful monsters attacked the surface, some explorers found the entrace to the cave. It was decided "
                 "that the most dashing, cunning, and powerful fighter would be sent to assault it. Unfortunately, you "
-                "were the only fighter around. After days of travel, you entered the cave.";
+                "were the only fighter around. After days of travel, you enter the cave.";
             break;
         case 2:
-            line << "After days of near constant fighting, you managed to descend to the next level. Instead of another "
-                "dungeon, you find yourself in a giant cave. You notice that as you go further into the earth, the "
-                "monsters seen to become stronger. You also notice that merchant's guild has somehow gained a "
+            line << "After days of near-constant fighting, you manage to descend to the next level. Instead of another "
+                "dungeon, you find yourself in a giant cave. You notice that as you descend further into the earth, the "
+                "monsters seem to become stronger. You also notice that merchant's guild has somehow gained a "
                 "monopoly on all trade within the monster economy.";
             break;
         case 3:
             line << "Weeks pass before you find the staircase to the next level, and after climbing down for half an hour, you "
                 "find a small town with an artificial sun overhead. Somehow, the merchant lobbyists managed to "
-                "convince the non-existant monster government that creating a town would stabilize the economy and "
+                "convince the nonexistent monster government that creating a town would stabilize the economy and "
                 "build a stronger sense of community.";
             break;
         case 4:
-            line << "The next floor, you find a forest. 'How the fuck did they manage to grow a forest a kilometer "
-                "underground?' you ask. 'Why is the Aztec god that created mankind here, and why are there several "
-                "copies of him?' Unfortunately, merchants are too busy haggling over the prices of human blood to "
+            line << "The next floor, you find a forest. \"How the fuck did they manage to grow a forest a kilometer "
+                "underground?\" you ask. \"Why is the Aztec god that created mankind here, and why are there several "
+                "copies of him?\" Unfortunately, merchants are too busy haggling over the prices of human blood to "
                 "reply, and none of the monsters can speak human.";
             break;
         default:
